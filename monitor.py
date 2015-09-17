34761ff5015c2d11d3991b55617dd243d895bb1e
Andreas Madsack <andreas@madflex.de>
2015-09-17 21:35:59 +0200

    send push via pushover if sensor hasn't pushed in the last 5 minutes

--- monitor.py
+++ monitor.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
-from config import API_TOKEN
+from config import API_TOKEN, PUSHOVER_CLIENT_TOKEN, PUSHOVER_API_TOKEN
+from pushover import Client
 
 
 def check():
@@ -12,13 +13,13 @@ def check():
         if node.get("last_data_push"):
             last_data_push = datetime.datetime.strptime(node.get("last_data_push")[:19],
                                                         "%Y-%m-%dT%H:%M:%S")
-            print(last_data_push)
-            # TODO:
-            # check if not older that 1h
-            # check if older than 5min
-            # if both; send message to owner
-            # - mail
-            # - push (i.e. pushover)
+            if last_data_push < datetime.datetime.utcnow() - datetime.timedelta(minutes=5):
+                if last_data_push > datetime.datetime.utcnow() - datetime.timedelta(hours=1):
+                    uid = node.get('uid')
+                    description = node.get('sensors', [{}])[0].get('description')
+                    client = Client(PUSHOVER_CLIENT_TOKEN, api_token=PUSHOVER_API_TOKEN)
+                    client.send_message("sensor: {}\ndescription: {}".format(uid, description),
+                                        title="Sensor hasn't pushed in the last 5 minutes!")
 
 
 check()
