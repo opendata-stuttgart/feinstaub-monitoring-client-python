4327484de51a7cd4a3494d1e28994ab76fa895f5
Andreas Madsack <andreas@madflex.de>
2015-09-17 22:12:52 +0200

    added click with option to show and/or push

--- monitor.py
+++ monitor.py
@@ -1,10 +1,14 @@
 import requests
 import datetime
+import click
 from config import API_TOKEN, PUSHOVER_CLIENT_TOKEN, PUSHOVER_API_TOKEN
 from pushover import Client
 
 
-def check():
+@click.command()
+@click.option('--push/--no-push', default=False)
+@click.option('--show/--no-show', default=True)
+def check(push, show):
     header = {'Authorization': 'Token ' + API_TOKEN}
     session = requests.Session()
     url = "https://api.dusti.xyz/v1/node/"
@@ -17,9 +21,12 @@ def check():
                 if last_data_push > datetime.datetime.utcnow() - datetime.timedelta(hours=1):
                     uid = node.get('uid')
                     description = node.get('sensors', [{}])[0].get('description')
-                    client = Client(PUSHOVER_CLIENT_TOKEN, api_token=PUSHOVER_API_TOKEN)
-                    client.send_message("sensor: {}\ndescription: {}".format(uid, description),
-                                        title="Sensor hasn't pushed in the last 5 minutes!")
+                    if push:
+                        client = Client(PUSHOVER_CLIENT_TOKEN, api_token=PUSHOVER_API_TOKEN)
+                        client.send_message("sensor: {}\ndescription: {}".format(uid, description),
+                                            title="Sensor hasn't pushed in the last 5 minutes!")
+                    if show:
+                        click.echo("{} | {:>35} | {}".format(last_data_push, uid, description))
 
-
-check()
+if __name__ == '__main__':
+    check()
