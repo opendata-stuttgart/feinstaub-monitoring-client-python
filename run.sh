465a242c24025920d66517df3e8247917b517bae
Andreas Madsack <andreas@madflex.de>
2015-10-10 20:50:08 +0200

    added locking using lock-files to not push twice per sensor

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 docker build --tag=feinstaub-monitor-client .
-docker run --rm -ti --name feinstaub-monitor-client-run feinstaub-monitor-client
+docker run --rm -ti --volume `pwd`/locks:/opt/code/locks --name feinstaub-monitor-client-run feinstaub-monitor-client
