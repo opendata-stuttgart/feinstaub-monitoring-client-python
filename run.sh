b9f7062aa003c78c48452653898908d7d77491d2
Andreas Madsack <andreas@madflex.de>
2015-10-11 21:31:31 +0200

    removed '-ti' for running as a cron

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 docker build --tag=feinstaub-monitor-client .
-docker run --rm -ti --volume `pwd`/locks:/opt/code/locks --name feinstaub-monitor-client-run feinstaub-monitor-client
+## add -ti when running in a shell and not as a cron
+docker run --rm --volume `pwd`/locks:/opt/code/locks --name feinstaub-monitor-client-run feinstaub-monitor-client
